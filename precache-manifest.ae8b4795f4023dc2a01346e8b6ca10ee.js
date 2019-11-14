4ba82180c0f34ac62fe3aef04c1064699631723f
Jack <jack@monkii.com>
2019-11-14 12:02:30 +1100

    Updates

---
+++ precache-manifest.ae8b4795f4023dc2a01346e8b6ca10ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a6409b8a72a85d2222d4eaffff244a",
+    "url": "/semaphore/index.html"
+  },
+  {
+    "revision": "40c3cbb7abb2cefc833f",
+    "url": "/semaphore/static/css/main.63da24c8.chunk.css"
+  },
+  {
+    "revision": "c32133c212d3f4d7e4b9",
+    "url": "/semaphore/static/js/2.8a0aa1a4.chunk.js"
+  },
+  {
+    "revision": "40c3cbb7abb2cefc833f",
+    "url": "/semaphore/static/js/main.3d74f1b8.chunk.js"
+  },
+  {
+    "revision": "493f8675bebe9334eb74",
+    "url": "/semaphore/static/js/runtime-main.37458e67.js"
+  }
+]);
